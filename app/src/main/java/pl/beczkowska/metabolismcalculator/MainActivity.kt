d058eabd830fb5d5c02f9bd60d405fdd2914cccd
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-04-20 18:06:40 +0200

    Initial commit

---
+++ app/src/main/java/pl/beczkowska/metabolismcalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.beczkowska.metabolismcalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
