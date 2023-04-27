36ed84e644518261fb47b33bd1fce00646d59436
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-04-27 16:58:11 +0200

    all works DONE

--- app/src/main/java/pl/beczkowska/metabolismcalculator/MainActivity.kt
+++ app/src/main/java/pl/beczkowska/metabolismcalculator/MainActivity.kt
@@ -24,87 +24,105 @@ class MainActivity : AppCompatActivity() {
             val weight = weightText.text.toString()
             val height = heightText.text.toString()
             val age = ageText.text.toString()
-            if(validation(weight, height, age)){
+            if (validation(weight, height, age)) {
 
-            //obliczanie bmi
-            val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat()/100))
+                //obliczanie bmi
+                val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat() / 100))
 
-            //wynik z dwoma miejscami po przecinku
-            val bmi2Digits = String.format("%.2f", bmi).toFloat()
-//
-//            //obliczanie metabolizmu
-//            val metabolism = ((10 * weight.toFloat()) +(6.25 *height.toFloat()) - (5 *age.toFloat()) +5)
-//            //wynik z dwoma miejscami po przecinku
-//            val metaboDigits = String.format("%", metabolism).toFloat()
-            Result(bmi2Digits)
-//            Result(metaboDigits)
+                //wynik z dwoma miejscami po przecinku
+                val bmi2Digits = String.format("%.2f", bmi).toFloat()
+
+                Result(bmi2Digits)
+
+            //obliczanie metabolizmu
+            val metabolism = ((10 * weight.toFloat()) +(6.25 *height.toFloat()) - (5 *age.toFloat()) +5)
+            //wynik bezprzecinkowy
+            val metaboDigits = String.format("%.0f", metabolism).toFloat()
+            Result2(metaboDigits)
         }}
 
-    }
-    private fun Result(bmi:Float){
-
-        // Pobranie referencji do pól TextView z pliku layoutu activity_main.xml
-        val bmival = findViewById<TextView>(R.id.bmivalue)
-        val normdescription = findViewById<TextView>(R.id.norm)
-        val textinf = findViewById<TextView>(R.id.normtext) //staly text
-//       val dailmetab = findViewById<TextView>(R.id.dailymetabo)
-//        val mataboval = findViewById<TextView>(R.id.metabolismvalue)
-
-
-        bmival.text = bmi.toString()
-        textinf.text = "haelthy range: 18.5-24.9"
-//        mataboval.text = metabolism.toString
-        var resultText = ""
-        var color = 0
-
-        when {
-            bmi<18.50 ->{
-                resultText = "UNDERWEIGHT"
-                color = R.color.under_weight
             }
-            bmi in 18.50..24.99->{
-                resultText = "HEALTHY"
-                color = R.color.healthy
-            }
-            bmi in 25.00..29.99->{
-                resultText = "OVERWEIGHT"
-                color = R.color.over_weight
-            }
-            bmi<29.99 ->{
-                resultText = "OBESE"
-                color = R.color.obese
-            }
+            private fun Result(bmi: Float) {
 
-        }
-        normdescription.setTextColor(ContextCompat.getColor(this,color))
-        normdescription.text = resultText
-    }
+                // Pobranie referencji do pól TextView z pliku layoutu activity_main.xml
+                val bmival = findViewById<TextView>(R.id.bmivalue)
+                val normdescription = findViewById<TextView>(R.id.norm)
+                val textinf = findViewById<TextView>(R.id.normtext) //staly text
 
-    //jesli waga wiek lub wzrost sa puste -> komunikat
-    private fun validation(weight:String?, height:String?, age:String?):Boolean{
-        return when{
-            height.isNullOrEmpty() -> {
-                Toast.makeText(this, "Height is empty. Enter your height", Toast.LENGTH_LONG).show()
-           return false
+                bmival.text = bmi.toString()
+                textinf.text = "haelthy range: 18.5-24.9"
+
+                var resultText = ""
+                var color = 0
+
+                when {
+                    bmi < 18.50 -> {
+                        resultText = "UNDERWEIGHT"
+                        color = R.color.under_weight
+                    }
+
+                    bmi in 18.50..24.99 -> {
+                        resultText = "HEALTHY"
+                        color = R.color.healthy
+                    }
+
+                    bmi in 25.00..29.99 -> {
+                        resultText = "OVERWEIGHT"
+                        color = R.color.over_weight
+                    }
+
+                    bmi >= 29.99 -> {
+                        resultText = "OBESE"
+                        color = R.color.obese
+                    }
+
+                }
+                normdescription.setTextColor(ContextCompat.getColor(this, color))
+                normdescription.text = resultText
             }
-            weight.isNullOrEmpty() -> {
-                Toast.makeText(this, "Weight is empty. Enter your weight", Toast.LENGTH_LONG).show()
-                return false
-        }
-            age.isNullOrEmpty() -> {
-                Toast.makeText(this, "Age is empty. Enter your age", Toast.LENGTH_LONG).show()
-                return false
-            }
-
-            else -> {
-            return true}
-        }
-
+    private fun Result2(metabolism :Float){
+        val dailmetab = findViewById<TextView>(R.id.dailymetabo)
+        val metaboval = findViewById<TextView>(R.id.metabolismvalue)
+        metaboval.text = metabolism.toString()
     }
 
 
+            //jesli waga wiek lub wzrost sa puste -> komunikat
+            private fun validation(weight: String?, height: String?, age: String?): Boolean {
+                return when {
+                    height.isNullOrEmpty() -> {
+                        Toast.makeText(
+                            this,
+                            "Height is empty. Enter your height",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        return false
+                    }
 
-}
+                    weight.isNullOrEmpty() -> {
+                        Toast.makeText(
+                            this,
+                            "Weight is empty. Enter your weight",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        return false
+                    }
+
+                    age.isNullOrEmpty() -> {
+                        Toast.makeText(this, "Age is empty. Enter your age", Toast.LENGTH_SHORT)
+                            .show()
+                        return false
+                    }
+
+                    else -> {
+                        return true
+
+                }
+                }}}
+
+
+
+
 /* TODO
         BMI
 MASA  CIALA(kg)/ WZROST^2
