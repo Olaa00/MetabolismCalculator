72333be3e81f2c77471bfb456843e527db314663
Ola00 <aleksandra.beczkowska00@gmail.com>
2023-04-27 14:55:04 +0200

    BMI all done + toast

--- app/src/main/java/pl/beczkowska/metabolismcalculator/MainActivity.kt
+++ app/src/main/java/pl/beczkowska/metabolismcalculator/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             val weight = weightText.text.toString()
             val height = heightText.text.toString()
             val age = ageText.text.toString()
+            if(validation(weight, height, age)){
 
             //obliczanie bmi
             val bmi = weight.toFloat() / ((height.toFloat() / 100) * (height.toFloat()/100))
@@ -35,7 +38,7 @@ class MainActivity : AppCompatActivity() {
 //            val metaboDigits = String.format("%", metabolism).toFloat()
             Result(bmi2Digits)
 //            Result(metaboDigits)
-        }
+        }}
 
     }
     private fun Result(bmi:Float){
@@ -43,16 +46,60 @@ class MainActivity : AppCompatActivity() {
         // Pobranie referencji do pól TextView z pliku layoutu activity_main.xml
         val bmival = findViewById<TextView>(R.id.bmivalue)
         val normdescription = findViewById<TextView>(R.id.norm)
-        val textinf = findViewById<TextView>(R.id.normtext)
+        val textinf = findViewById<TextView>(R.id.normtext) //staly text
 //       val dailmetab = findViewById<TextView>(R.id.dailymetabo)
 //        val mataboval = findViewById<TextView>(R.id.metabolismvalue)
 
 
         bmival.text = bmi.toString()
-        textinf.text = "normal range 18.5-24.9"
+        textinf.text = "haelthy range: 18.5-24.9"
 //        mataboval.text = metabolism.toString
         var resultText = ""
         var color = 0
+
+        when {
+            bmi<18.50 ->{
+                resultText = "UNDERWEIGHT"
+                color = R.color.under_weight
+            }
+            bmi in 18.50..24.99->{
+                resultText = "HEALTHY"
+                color = R.color.healthy
+            }
+            bmi in 25.00..29.99->{
+                resultText = "OVERWEIGHT"
+                color = R.color.over_weight
+            }
+            bmi<29.99 ->{
+                resultText = "OBESE"
+                color = R.color.obese
+            }
+
+        }
+        normdescription.setTextColor(ContextCompat.getColor(this,color))
+        normdescription.text = resultText
+    }
+
+    //jesli waga wiek lub wzrost sa puste -> komunikat
+    private fun validation(weight:String?, height:String?, age:String?):Boolean{
+        return when{
+            height.isNullOrEmpty() -> {
+                Toast.makeText(this, "Height is empty. Enter your height", Toast.LENGTH_LONG).show()
+           return false
+            }
+            weight.isNullOrEmpty() -> {
+                Toast.makeText(this, "Weight is empty. Enter your weight", Toast.LENGTH_LONG).show()
+                return false
+        }
+            age.isNullOrEmpty() -> {
+                Toast.makeText(this, "Age is empty. Enter your age", Toast.LENGTH_LONG).show()
+                return false
+            }
+
+            else -> {
+            return true}
+        }
+
     }
 
 
@@ -61,6 +108,10 @@ class MainActivity : AppCompatActivity() {
 /* TODO
         BMI
 MASA  CIALA(kg)/ WZROST^2
+<18,5	niedowaga
+18,5 – 24,9	masa ciała prawidłowa
+25,0 – 29,9 	nadwaga
+30 – 34,9	I stopień otyłości
        TODO METABOLISM
 PPM [kcal] = (10 ×masa ciała w kg) + (6,25 × wzrost w cm) – (5 × wiek w latach) + 5
 */
